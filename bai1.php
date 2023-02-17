969a143f22ddb7814c39a4a241586f1d3c6fddf9
= <116275520+HoXuanTy@users.noreply.github.com>
2023-02-17 08:45:47 +0700

    suabai1

--- bai1.php
+++ bai1.php
@@ -1,32 +1,45 @@
 <?php
 # Cho 1 mảng gồm n phần tử là các số nguyên, 
-# hãy in ra màn hình số lớn nhất và nhỏ nhất của mảng. Cho biết vị trí của các phần tử đó
 
 // Max
 function getMax($array)
 {
    $n = count($array);
+   $vtmax="";
+   $vtm =0;
    $max = $array[0];
-   for ($i = 1; $i < $n; $i++)
-       if ($max < $array[$i])
-           $max = $array[$i];
-    return $max;      
+   for ($i = 1; $i < $n; $i++){
+       if ($max < $array[$i]){
+            $max = $array[$i];
+            $vtmx = $i;
+       }
+    }
+    $vtmax= "So lon nhat la ".$max." tai vi tri ".$vtmx;
+    return $vtmax;      
 }
 
 // Min
 function getMin($array)
 {
    $n = count($array);
+   $vtmin="";
+   $vtm =0;
    $min = $array[0];
-   for ($i = 1; $i < $n; $i++)
-       if ($min > $array[$i])
-           $min = $array[$i];
-    return $min;      
+   for ($i = 1; $i < $n; $i++){
+       if ($min > $array[$i]){
+            $min = $array[$i];
+            $vtm = $i;
+       }
+           
+    }
+    
+    $vtmin= "So nho nhat la ".$min." tai vi tri ".$vtm;       
+    return $vtmin;      
 }
 
 
-$array = array(1, 2, 3, 4, 5);
-echo(getMax($array));
-echo("\n");
+$array = array(3,2,8,7,8,9,1);
+echo (getMax($array));
+echo("<br>");
 echo(getMin($array));
 ?>
--- login.php
+++ login.php
@@ -13,24 +13,30 @@
 <body>
 <div class="wrapper fadeInDown">
   <div id="formContent">
-    <!-- Tabs Titles -->
-
-    <!-- Icon -->
-    <div class="fadeIn first">
-      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
-    </div>
 
     <!-- Login Form -->
     <form>
       <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+      <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
+      <input type="submit" name="btn" class="fadeIn fourth" value="Log In">
     </form>
 
     <!-- Remind Passowrd -->
     <div id="formFooter">
       <a class="underlineHover" href="#">Forgot Password?</a>
     </div>
+  <?php
+   if (isset($_POST['btn'])) {
+    $UserName = $_POST['login'];
+    $Password = $_POST['password'];
+    if ($UserName == 'hoxuanty' && $Password == 'ty12345') {
+        echo "Đăng nhập thành công";
+    } else {
+        echo "Đăng nhập thất bại !";
+    }
+}
+  
+?>
 
   </div>
 </div>
